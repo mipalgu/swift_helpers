e7f2631ea2baf530e139d690eee31e904260765d
Callum McColl <callum.mccoll1@gmail.com>
2018-05-21 12:30:23 +1000

    Removed library type on products

--- Package.swift
+++ Package.swift
@@ -9,16 +9,13 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "swift_helpers",
-            type: .dynamic,
             targets: ["swift_helpers"]),
         .library(
             name: "Functional",
-            type: .dynamic,
             targets: ["Functional"]
         ),
         .library(
             name: "IO",
-            type: .dynamic,
             targets: ["IO"]
         )
     ],
